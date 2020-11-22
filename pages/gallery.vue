0606341ff6f6a694cf79dd1a9e83dc82cb37f2ae
ed85w <ed.walker@hotmail.co.uk>
2020-11-22 12:01:49 +0000

    testing iphone gallery issue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -24,18 +24,20 @@
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6" data-aos="flip-left" data-aos-duration="1200" data-aos-offset="200">
-          <div class="polaroid polaroid3">
-            <img src="~static/img/dog-3.jpeg" alt="willow the dog with a birthday hat on">
-            <div class="caption font-cursive">
-              "Our pooch loves Zoe!!! She is so friendly and approachable. I would highly recommend Happy Trails Pet Services."
-            </div>
-            <div class="testimonial">
-              Carly
+        <div class="col-12 col-md-6">
+          <div class="polaroid-holder" data-aos="flip-left" data-aos-duration="1200" data-aos-offset="200">
+            <div class="polaroid polaroid3">
+              <img src="~static/img/dog-3.jpeg" alt="willow the dog with a birthday hat on">
+              <div class="caption font-cursive">
+                "Our pooch loves Zoe!!! She is so friendly and approachable. I would highly recommend Happy Trails Pet Services."
+              </div>
+              <div class="testimonial">
+                Carly
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6" data-aos="flip-right" data-aos-duration="1200" data-aos-delay="600" data-aos-offset="200">
+        <div class="col-12 col-md-6" data-aos="flip-up" data-aos-duration="1200" data-aos-delay="600" data-aos-offset="200">
           <div class="polaroid polaroid4">
             <img src="~static/img/dog-4.png" alt="zoe from happy trails pet services with 2 dogs">
             <div class="caption font-cursive">
